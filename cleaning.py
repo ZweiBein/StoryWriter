09983658dd39040946fd72956b607297464d1d59
UrbanSkurk <UrbanSkurk@HELL>
2018-03-20 09:48:21 +0100

    Removed som weights and allowed the script to include commas in patterns

--- cleaning.py
+++ cleaning.py
@@ -1,3 +1,5 @@
 def remove_non_ascii(s):
-    return "".join(i for i in s if 97 <= ord(i) <= 128 or ord(i) == 32 or i == '.' or i == '!' or i == '?')
+    return "".join(i for i in s if 97 <= ord(i) <= 128 or ord(i) == 32 or i == '.'
+                   or i == '!' or i == '?' or i == ',')
+
 
--- main.py
+++ main.py
@@ -45,8 +45,8 @@ model = Sequential()
 model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
-filename = R"Model\weights-improvement-32-1.5288.hdf5"
-model.load_weights(filename)
+# filename = R"Model\weights-improvement-46-1.3750.hdf5"
+# model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # define the checkpoint
 filepath=R"Model\weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
